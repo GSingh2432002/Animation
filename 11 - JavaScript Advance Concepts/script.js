b6d1ec964f0c3729d1bc46156dde9adc7d7362f9
Gaurav Kumar Singh <gsingh332211@gmail.com>
2025-01-27 12:17:51 +0530

    Advance JS Completed

---
+++ 11 - JavaScript Advance Concepts/script.js
@@ -0,0 +1,9 @@
+// Higher Order Functions
+// Contructor Functions
+// first class functions
+// new keyword
+// iife
+// prototype
+// prototypal inheritance
+// this call apply bind
+// pure & impure functions
